11421d73f0f76db78c6ec2f01d5a097a0df60839
froze <creehuis@icloud.com>
2024-07-22 22:48:25 +0200

    more stff

--- src/main/java/com/therium/Therium.java
+++ src/main/java/com/therium/Therium.java
@@ -6,7 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Therium implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String 	MOD_ID = "therium";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/com/therium/TheriumModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.therium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheriumModDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
